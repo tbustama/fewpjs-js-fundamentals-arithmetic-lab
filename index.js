6c51a5837dbe45b8e712156879df57a1bca654db
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-18 08:39:33 -0800

    commit tree d6a026454be61a542e1c124d393d30c2f63846b1

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const mod = 14 % 5;
+
+const max = Math.max(20, 4, 5, 6);
